ac35ecdf27203a4127c69666f28aacaa3fb20198
Hashmu0786 <128887812+Hashmu0786@users.noreply.github.com>
2023-04-27 06:54:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+let string = "";
+let buttons = document.querySelectorAll('.btn');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('.screen').value = string;
+    }
+    else if(e.target.innerHTML == 'C'){
+      string = ""
+      document.querySelector('.screen').value = string;
+    }
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('.screen').value = string;
+      }
+  })
+})
